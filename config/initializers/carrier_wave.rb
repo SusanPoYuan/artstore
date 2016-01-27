8e2165d2c7cba293562756b9bbda9efc97a5ce05
PoYuan Huang <reticentminds@gmail.com>
2016-01-27 14:41:38 +0800

    s3 & fog done

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,15 @@
+CarrierWave.configure do |config|
+  if Rails.env.production? 
+    config.storage :fog                       
+    config.fog_credentials = {
+      provider:              'AWS',                        
+      aws_access_key_id:     ENV["aws_access_key_id"],      # 你的 key           
+      aws_secret_access_key: ENV["aws_access_key_id"],      # 你的 secret key         
+      region:                'Tokyo' # 你的 S3 bucket 的 Region 位置   
+    }
+    config.fog_directory  = 'poyuantest1' # 你設定的 bucket name 
+
+  else
+    config.storage :file
+  end
+end
